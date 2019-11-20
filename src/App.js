8783269a09b5f7b242a4ac9fb0013b6ff1618d8d
Sergey Antonov <flyker777@yandex.ru>
2019-11-20 20:51:25 +0300

    update login form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Button, Block } from './components';
+import { Auth } from './pages';
 
 function App() {
   return (
     <div className="wrapper">
-      <Block>
-        <h1>Hello</h1>
-        <Button type="primary" size="large">This is Button</Button>
-      </Block>
+      <Auth />
     </div>
   );
 }
