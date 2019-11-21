18907cd27f996f55734fad137727fd3b1464213d
Sergey Antonov <flyker777@yandex.ru>
2019-11-21 22:37:40 +0300

    add attachments

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -8,26 +8,35 @@ import noReadedSvg from "assets/img/noreaded.svg";
 
 import './Message.scss';
 
-const Message = ({ avatar, user, text, date, isMe, isReaded }) => (
+const Message = ({ avatar, user, text, date, isMe, isReaded, attachments }) => (
   <div className={classNames('message', { 'message--isme': isMe })}>
     <div className="message__content">
       {isMe && isReaded ? (
-      <img 
-        className="message__icon-readed" 
-        src={readedSvg} 
-        alt="Readed Icon" 
-      />) : (
-        <img 
-        className="message__icon-readed" 
-        src={noReadedSvg} 
-        alt="No readed Icon" 
-      />)}
+        <img
+          className="message__icon-readed"
+          src={readedSvg}
+          alt="Readed Icon"
+        />) : (
+          <img
+            className="message__icon-readed"
+            src={noReadedSvg}
+            alt="No readed Icon"
+          />)}
       <div className="message__avatar">
         <img src={avatar} alt={`Avatar ${user.fullname}`} />
       </div>
       <div className="message__info">
         <div className="message__bubble">
           <p className="message__text">{text}</p>
+        </div>
+        <div className="message__attachments">
+          {
+            attachments && attachments.map(item => (
+              <div className="message__attachments-item">
+                <img src={item.url} alt={item.filename} />
+              </div>
+            ))
+          }
         </div>
         <span className="message__date">{formatDistanceToNow(date, { addSuffix: true, locale: ruLocale })}</span>
       </div>
@@ -44,7 +53,8 @@ Message.propTypes = {
   avatar: PropTypes.string,
   text: PropTypes.string,
   date: PropTypes.object,
-  user: PropTypes.object
+  user: PropTypes.object,
+  attachments: PropTypes.array,
 };
 
 export default Message;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,14 +10,28 @@ const Home = () => (
       isMe={false} 
       avatar="https://sun9-21.userapi.com/c831408/v831408737/a797b/AOuegaEIaeA.jpg?ava=1" 
       text="Салам Бро! Салам Бро! Салам Бро! Салам Бро!Салам Бро!Салам Бро!" 
-      date={new Date(2019, 10, 21, 18, 0, 0)} 
+      date={new Date(2019, 10, 21, 18, 0, 0)}
+      attachments={[
+        {
+          filename: 'image.jpg',
+          url: 'https://source.unsplash.com/100x100/?random=1&nature,water'
+        },
+        {
+          filename: 'image.jpg',
+          url: 'https://source.unsplash.com/100x100/?random=2&nature,water'
+        },
+        {
+          filename: 'image.jpg',
+          url: 'https://source.unsplash.com/100x100/?random=3&nature,water'
+        }
+      ]}
     />
     <Message 
       isMe={true}
-      isReaded={false}
+      isReaded={true}
       avatar="https://sun9-11.userapi.com/c855124/v855124025/c074c/M2uI9C1Xpts.jpg?ava=1" 
       text="Hello me!" 
-      date={new Date(2019, 10, 21, 18, 50, 0)} 
+      date={new Date(2019, 10, 21, 18, 50, 0)}
     />
   </section>
 );
